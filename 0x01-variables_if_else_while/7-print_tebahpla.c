b4ec07c38649e3127f591fe072c7b6bbc24549b9
Mostafa Elkaranshawy <110842052+MostafaElKaranshawy@users.noreply.github.com>
2023-03-17 23:58:28 +0200

    Update 7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -15,38 +15,4 @@ int main(void)
 	putchar('\n');
 
 	return (0);
-}tdio.h>
- /**
-  * main - main block
-  * Description: Print the alphabet in reverse order in lowercase.
-  * Return: 0
-  */
-int main(void)
-{
-  char c;
-
-  for (c = 'z'; c >= 'a'; c--)
-    {
-      putchar(c);
-    }
-  putchar('\n');
-
-  return (0);
-}~#include <stdio.h>
-/**
- * main - main block
- * Description: Print the alphabet in reverse order in lowercase.
- * Return: 0
- */
-int main(void)
-{
-	char c;
-
-	for (c = 'z'; c >= 'a'; c--)
-	{
-		putchar(c);
-	}
-	putchar('\n');
-
-	return (0);
 }
