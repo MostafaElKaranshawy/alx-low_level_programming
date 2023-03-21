05a900aa60c24c9eb391f64f51c650bef0a29803
Mostafa Elkaranshawy <110842052+MostafaElKaranshawy@users.noreply.github.com>
2023-03-21 12:46:59 +0200

    Update 101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -6,13 +6,13 @@
  */
 int main(void)
 {
-  int sum = 0;
-  
-  for(int i = 0; i <= 1024; i++)
-    {
-      if(i % 3 == 0 || i % 5 ==0)
-	sum += i;
-    }
-  printf("%d", sum);
-  return (0);
+	int i, sum = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 5) == 0 || (i % 3) == 0)
+			sum += i;
+	}
+	printf("%d\n", sum);
+	return (0);
 }
