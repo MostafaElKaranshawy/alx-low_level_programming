0201e383fd7b6d5e35e0983de92c3ad679c703d9
Mostafa Elkaranshawy <110842052+MostafaElKaranshawy@users.noreply.github.com>
2023-03-21 11:50:42 +0200

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * Ascii charachters
- * print _putchar followed by new line 
-*/
+ * main -  prints _putchar, followed by a new line
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {
