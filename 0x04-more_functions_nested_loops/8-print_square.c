910713909db9f22044f0bda1d4a88344256659d7
Mostafa Elkaranshawy <110842052+MostafaElKaranshawy@users.noreply.github.com>
2023-03-24 00:03:01 +0200

    Update 8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -12,7 +12,9 @@ void print_square(int size)
 	for ( i = 0; i < size; i++)
 	{
 		for ( j = 0; j < size; j++)
-		    _putchar(35);
+		{
+			_putchar(35);
+		}
 		if( i != size - 1 )
 			_putchar('\n');
 	}
