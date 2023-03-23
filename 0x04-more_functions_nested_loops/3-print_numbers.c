1c4d4802ee6299300c389fac135d082a4ab89ecf
Mostafa Elkaranshawy <mostafaelkaranshawy0@gmail.com>
2023-03-23 07:48:16 -0700

    s

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print numbers - 0 : 9
+ * Return: no return
+ */
+void print_numbers()
+{
+  int i;
+
+  for( i = 48; i < 58; i++)
+    {
+      _putchar(i);
+    }
+
+}
+  
